dd874143e29449cefae291bf3e6952b794708de2
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-09-29 12:21:12 -0400

    UPDATE: Global Files

--- src/components/ui/globals/Header.tsx
+++ src/components/ui/globals/Header.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { DominantHand, ThemeToggle } from '@/components';
+import { DominantHand, ThemeToggle, HydrationSafe } from '@/components';
 
 export function Header() {
 	return (
 		<header className='Header flex justify-between items-center border-b border-black p-1'>
-			<DominantHand />
+			<HydrationSafe fallback={<div className='w-10 h-10' />}>
+				<DominantHand />
+			</HydrationSafe>
 			<h1 className='text-2xl font-bold'>RandoDrum</h1>
 			<ThemeToggle />
 		</header>
--- src/components/ui/sections/GenerateBeat/index.ts
+++ src/components/ui/sections/GenerateBeat/index.ts
@@ -1,5 +1,4 @@
 export { BeatsField } from './BeatsField';
-export { BeatForm } from './BeatForm';
 export { DifficultyField } from './DifficultyField';
 export { GenerateBeat } from './GenerateBeat';
 export { GenerateBeatButton } from './GenerateBeatButton';
