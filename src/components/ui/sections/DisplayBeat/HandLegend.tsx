f62f81c968ccca113848f9fd8d28a1a079f4586c
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-11-04 00:45:59 -0500

    UPDATE: Display Components

--- src/components/ui/sections/DisplayBeat/HandLegend.tsx
+++ src/components/ui/sections/DisplayBeat/HandLegend.tsx
@@ -5,7 +5,7 @@ export function HandLegend() {
 	];
 
 	return (
-		<div className='HandLegend flex justify-around gap-6 border-t border-light-gray text-gray text-xs pt-4'>
+		<div className='HandLegend text-gray flex justify-around gap-6 text-xs'>
 			{hands.map(([name, color]) => (
 				<div key={name} className={`${name}Hand flex items-center gap-1`}>
 					<span className={`text-${color} font-bold`}>{name[0]}</span>
